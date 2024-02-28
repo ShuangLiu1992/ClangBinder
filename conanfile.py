a85be845d1387aed81e4432b08b6849a96fecb79
ShuangLiu1992 <ShuangLiu1992@users.noreply.github.com>
2024-02-28 15:46:47 +0000

    cmake and conan

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain
+import os
+
+
+class ClangBinderonan(ConanFile):
+    name = "clang_binder"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires("llvm/18.1.0-rc3@")
