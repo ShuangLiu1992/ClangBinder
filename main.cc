33f65195711a0cd3dd6f5c7313b95b1e95505a25
ShuangLiu <liushuang.work@gmail.com>
2024-02-29 23:03:10 +0000

    CLANG_INCLUDE_DIRS

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <clang/AST/Comment.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Basic/Diagnostic.h>
+#include <clang/Basic/SourceLocation.h>
+#include <clang/Frontend/CompilerInstance.h>
+
+int main()
+{
+    return 1;
+}
