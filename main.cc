b3da1785dbee94aaed2a69aea30391a4c3761c91
ShuangLiu <liushuang.work@gmail.com>
2024-03-02 21:28:16 +0000

    Tooling

--- main.cc
+++ main.cc
@@ -3,8 +3,17 @@
 #include <clang/Basic/Diagnostic.h>
 #include <clang/Basic/SourceLocation.h>
 #include <clang/Frontend/CompilerInstance.h>
+#include <clang/Tooling/CommonOptionsParser.h>
 
-int main()
+int main(int argc, const char** argv)
 {
+    llvm::cl::SetVersionPrinter([](llvm::raw_ostream& OS)
+    {
+        OS << "LLVM " << LLVM_VERSION_MAJOR << "." << LLVM_VERSION_MINOR <<
+            "." << LLVM_VERSION_PATCH << "\n";
+    });
+    llvm::cl::OptionCategory category{"ClangBinder"};
+    llvm::Expected<clang::tooling::CommonOptionsParser> eop = clang::tooling::CommonOptionsParser::create(
+        argc, argv, category);
     return 1;
 }
