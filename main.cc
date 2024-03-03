8a0b30bd15b2af0d543855c302cbd2e67067d029
ShuangLiu <liushuang.work@gmail.com>
2024-03-03 00:38:55 +0000

    compile database

--- main.cc
+++ main.cc
@@ -6,6 +6,35 @@
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
 
+llvm::cl::OptionCategory category{"ClangBinder"};
+
+
+class BinderASTConsumer : public clang::ASTConsumer
+{
+public:
+    // override the constructor in order to pass CI
+    explicit BinderASTConsumer(clang::CompilerInstance* ci)
+    {
+    }
+
+    // override this to call our ExampleVisitor on the entire source file
+    virtual void HandleTranslationUnit(clang::ASTContext& context)
+    {
+        context.getTranslationUnitDecl();
+    }
+};
+
+class BinderFrontendAction : public clang::ASTFrontendAction
+{
+public:
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance& ci, llvm::StringRef file)
+    {
+        llvm::outs() << "Process input file " << file << "\n";
+        return std::unique_ptr<clang::ASTConsumer>(new BinderASTConsumer(&ci));
+    }
+};
+
+
 int main(int argc, const char** argv)
 {
     llvm::cl::SetVersionPrinter([](llvm::raw_ostream& OS)
@@ -13,9 +42,11 @@ int main(int argc, const char** argv)
         OS << "LLVM " << LLVM_VERSION_MAJOR << "." << LLVM_VERSION_MINOR <<
             "." << LLVM_VERSION_PATCH << "\n";
     });
-    llvm::cl::OptionCategory category{"ClangBinder"};
-    llvm::Expected<clang::tooling::CommonOptionsParser> eop = clang::tooling::CommonOptionsParser::create(
-        argc, argv, category);
-    clang::tooling::ClangTool tool(eop->getCompilations(), eop->getSourcePathList());
+    if (auto eop = clang::tooling::CommonOptionsParser::create(
+        argc, argv, category))
+    {
+        clang::tooling::ClangTool tool(eop->getCompilations(), eop->getSourcePathList());
+        tool.run(clang::tooling::newFrontendActionFactory<BinderFrontendAction>().get());
+    }
     return 1;
 }
